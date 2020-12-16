0eeb59bdd5a17967a5082cfb39fa9a5d633ebd74
STS453 <venkatesan.a@w3magix.com>
2020-12-16 11:26:19 +0530

    commit

--- app/src/main/java/com/example/signinandsignup/MainActivity.java
+++ app/src/main/java/com/example/signinandsignup/MainActivity.java
@@ -39,21 +39,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean save() {
+
+        //Inittialisation of fthe Realm and getDefaultInstance____________________________________//
         Realm.init(MainActivity.this);
         Realm realmInstance = Realm.getDefaultInstance();
+
+        //Transactions__________________________________________//
         if (!realmInstance.isInTransaction()) {
             realmInstance.beginTransaction();
         }
 
+       //move to next table_id Querry___________________________________________________//
         int primary = 0;
         if (realmInstance.where(GetAndSet.class) != null && realmInstance.where(GetAndSet.class).max("id") != null) {
             primary = realmInstance.where(GetAndSet.class).max("id").intValue() + 1;
         }
 
+        //Create table structure and enter the data values___________________________________________________________________//
         GetAndSet row = realmInstance.createObject(GetAndSet.class, primary);
         row.setEmail(email.getText().toString());
         row.setPassword(password.getText().toString());
+
+        //commitTrasaction_______________________________________//
         realmInstance.commitTransaction();
+        //close____________________________________//
         if (!realmInstance.isClosed()) {
             realmInstance.close();
         }
